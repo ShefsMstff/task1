ba1cf28518a8f0307d292cabe4b7a20c85014521
ShefsMstff <sheffmustafaeff@gmail.com>
2023-04-26 13:58:04 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$("#off").on("click", function(){
+    $("#slide").hide("slow")
+}
+)
+$("#on").on("click", function(){
+    $("#slide").show("fast")
+        $("#slide").css("backgroundColor" , "pink")
+    }
+    )
+    
+
+
